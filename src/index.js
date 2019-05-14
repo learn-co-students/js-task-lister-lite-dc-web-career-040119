cea4018a3ffef9a5b67f3cb828fcacd2134aee62
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-05-14 18:35:08 -0400

    we're donegit add .! yaygit add .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let taskForm = document.getElementById("create-task-form")
+  const tasks = document.getElementById("tasks");
+  taskForm.addEventListener("submit", createNewTask);
 });
+
+function createNewTask(e){
+  e.preventDefault()
+  let taskCreation = document.createElement('li');
+  let newTask = document.getElementById("new-task-description")
+  taskCreation.innerText = newTask.value
+  tasks.appendChild(taskCreation)
+  // console.log(taskCreation)
+}
+
+
+// button = document.getElementById("create-task-form")
+//
+// button.addEventListener("click", grab)
+
+// function grab(e) {
+//   e.preventDefault
+// }
