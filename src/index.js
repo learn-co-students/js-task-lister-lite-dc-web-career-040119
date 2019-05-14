d65165c2b4393444605a2db356d845cf35bddbbd
Justin <justinhwu95@gmail.com>
2019-05-14 16:45:49 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById("create-task-form")
+
+  form.addEventListener("submit", function(event){
+    let name = document.getElementById("new-task-description").value
+    event.preventDefault()
+    createList(name)
+    event.target.reset()
+  }
+
+  )
 });
+
+function createList(input){
+  let unordered_list = document.getElementById("tasks")
+  let list_element = document.createElement("li")
+  list_element.innerHTML = `${input}`
+  unordered_list.appendChild(list_element)
+ }
